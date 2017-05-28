38970225485f67bfb2d8a28a4415d7b550f40276
George Kosmidis <georgekosmidis@outlook.com>
2017-05-28 16:33:42 +0300

    Abstract trainer

---
+++ SimpleNeuralNetwork/Trainers/AbstactTrainer.cs
@@ -0,0 +1,101 @@
+﻿using SimpleNeuralNetwork.AI;
+using SimpleNeuralNetwork.AI.Models;
+using SimpleNeuralNetwork.EventArgumens;
+using SimpleNeuralNetwork.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleNeuralNetwork.Trainers
+{
+    public abstract class AbstactTrainer
+    {
+        NeuralNetworkCompute _neuralNetworkCompute;
+        NeuralNetwork _nueralNetwork = new NeuralNetwork();
+        IDataHandle _filehandle;
+
+        protected virtual double[][] inputData { get; } = new double[][] { new double[] { } };
+        protected virtual double[][] resultsData { get; } = new double[][] { new double[] { } };
+
+
+        public delegate void StatusUpdateHandler(object sender, ProgressEventArgs e);
+        public event StatusUpdateHandler OnUpdateStatus;
+
+        public AbstactTrainer(NeuralNetworkCompute neuralNetworkCompute, IDataHandle filehandle)
+        {
+            _neuralNetworkCompute = neuralNetworkCompute;
+            _filehandle = filehandle;
+        }
+        public void Save(string filename)
+        {
+            _filehandle.Save<NeuralNetwork>(filename, _nueralNetwork);
+        }
+        public void Train(int hiddenLayerNeurons, double acceptedError)
+        {
+            _neuralNetworkCompute.CreateLayers(inputData[0].Length, hiddenLayerNeurons, resultsData[0].Length);
+
+            var j = 0;
+            var leastError = 1d;
+            do
+            {
+                var s = GetMatrixHeaders(j + 1);
+                OnUpdateStatus?.Invoke(this, new ProgressEventArgs(s));
+
+                var innerLeastError = 0d;
+                for (int i = 0; i < inputData.Length; i++)
+                {
+                    _nueralNetwork = _neuralNetworkCompute.Train(inputData[i], resultsData[i]);
+
+                    var status = GetMatrix(_nueralNetwork.OutputNeurons, resultsData[i]);
+                    OnUpdateStatus?.Invoke(this, new ProgressEventArgs(status));
+
+                    innerLeastError = GetMaxError(_nueralNetwork.OutputNeurons);
+                }
+                leastError = Math.Min(leastError, innerLeastError);
+
+            } while (leastError > acceptedError);
+
+        }
+
+        private double GetMaxError(List<Neuron> neurons)
+        {
+            var maxError = 0d;
+            foreach (var neuron in neurons)
+            {
+                maxError = Math.Max(maxError, Math.Abs(neuron.Error));
+            }
+            return maxError;
+        }
+
+        private string GetMatrixHeaders(int iteration)
+        {
+            var s = new StringBuilder();
+            s.AppendLine(new String('*', 50));
+            s.AppendLine("Starting Iteration " + iteration);
+            s.AppendLine(new String('-', 50));
+            s.AppendLine(String.Format("{0,10} |{1,10} |{2,10} |{3,10}", "Neuron", "Expected", "Actual", "Error"));
+            return s.ToString();
+        }
+        private string GetMatrix(List<Neuron> neurons, double[] expectedVaues)
+        {
+            var s = new StringBuilder();
+            var i = 0;
+            foreach (var neuron in neurons)
+            {
+                s.Append(
+                String.Format("{0,10} |{1,10} |{2,10} |{3,10}",
+                               (i + 1),
+                               expectedVaues[i++].ToString("0.0000", CultureInfo.InvariantCulture),
+                               neuron.Value.ToString("0.0000", CultureInfo.InvariantCulture),
+                               neuron.Error.ToString("0.0000", CultureInfo.InvariantCulture)
+                            )
+                );
+                s.Append(Environment.NewLine);
+            }
+            return s.ToString();
+        }
+    }
+}
--- SimpleNeuralNetwork/Trainers/AdditionTrainer.cs
+++ SimpleNeuralNetwork/Trainers/AdditionTrainer.cs
@@ -10,14 +10,10 @@ using System.Threading.Tasks;
 
 namespace SimpleNeuralNetwork.Trainers
 {
-    public class AdditionTrainer : ITrainer
+    public class AdditionTrainer : AbstactTrainer, ITrainer
     {
-        NeuralNetworkCompute _neuralNetworkCompute;
-        NeuralNetwork _nueralNetwork = new NeuralNetwork();
-        IDataHandle _filehandle;
-
         //Input data, defines variables inserted into the system, second dimension defines number of input neurons, for this case 2
-        private double[][] inputData = new double[][] {
+        protected override double[][] inputData { get; } = new double[][] {
                                 new double[] { .1, .2 },
                                 new double[] { .3, .1 },
                                 new double[] { .1, .4 },
@@ -25,55 +21,16 @@ namespace SimpleNeuralNetwork.Trainers
                             };
 
         //Expected output data defines expected result, second dimension defines number of output neurons, for this case 1
-        private double[][] resultsData = new double[][] {
+        protected override double[][] resultsData { get; } = new double[][] {
                                    new double[] { .3 },
                                    new double[] { .4 },
                                    new double[] { .5 },
                                    new double[] { .2 },
                                };
 
-        public delegate void StatusUpdateHandler(object sender, ProgressEventArgs e);
-        public event StatusUpdateHandler OnUpdateStatus;
-
-        public AdditionTrainer(NeuralNetworkCompute neuralNetworkCompute, IDataHandle filehandle)
+        public AdditionTrainer(NeuralNetworkCompute neuralNetworkCompute, IDataHandle filehandle) : base(neuralNetworkCompute, filehandle)
         {
-            _neuralNetworkCompute = neuralNetworkCompute;
-            _filehandle = filehandle;
-        }
-        public void Save(string filename)
-        {
-            _filehandle.Save<NeuralNetwork>(filename, _nueralNetwork);
-        }
-        public void Train(int hiddenLayerNeurons, double acceptedError)
-        {
-            //Create a neuron network with 2 input neurons, 5 hidden neurons and 1 output neuron
-            _neuralNetworkCompute.CreateLayers(inputData[0].Length, hiddenLayerNeurons, resultsData[0].Length);
-            //Train
-            var j = 0;
-            var leastError = 1d;
-            do//could be smaller but training data are few and makes no point...
-            {
-                OnUpdateStatus?.Invoke(this, new ProgressEventArgs("Iteration : " + (j++)));
 
-                var innerLeastError = 0d;
-                for (int i = 0; i < inputData.Length; i++)
-                {
-                    _nueralNetwork = _neuralNetworkCompute.Train(inputData[i], resultsData[i]);
-
-                    var status = "Output : " + _nueralNetwork.OutputNeurons[0].Value.ToString("0.000000") + " " +
-                                 "Expected : " + resultsData[i][0].ToString("0.000000") + " " +
-                                 "Error : " + _nueralNetwork.OutputNeurons[0].Error.ToString("0.000000") + " ";
-                    //ouput has only one neuron
-                    OnUpdateStatus?.Invoke(this, new ProgressEventArgs(status));
-
-                    innerLeastError = Math.Max(innerLeastError, Math.Abs(_nueralNetwork.OutputNeurons[0].Error));
-                }
-                leastError = Math.Min(leastError, innerLeastError);
-
-                OnUpdateStatus?.Invoke(this, new ProgressEventArgs("*************************"));
-            } while (leastError > acceptedError);
-
-            OnUpdateStatus?.Invoke(this, new ProgressEventArgs("Done after " + j + " iterations..."));
         }
     }
 }
